126b5c24d657d31de417d57e0527da86e9ae2e95
w3bgir1 <akulshyna@gmail.com>
2019-02-13 13:30:36 +0100

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,17 @@
+function doesNotPassAllValidations(name, msg) {
+    if (!name || !msg) {
+      alert('You forgot to fill in your name or message!');
+      return true;
+    }
+  
+    if(msg.length > 280) {
+      alert('Your comment is too long');
+      return true;
+    }
+    return false;
+  }
+  
+
 const submitComment = () => {
 
     const inputField = document.getElementById('name');
@@ -15,9 +29,15 @@ const submitComment = () => {
     comment.appendChild(h3);
     comment.appendChild(p);
 
+    if(doesNotPassAllValidations(name, msg)){
+        return null;
+      }
+
     const commentSection = document.getElementById('comments');
     commentSection.appendChild(comment);
+
     inputField.value = null;
     textArea.value = null;
+
 }
 
